b3fde1c1a424cdfdf75d47719df891abbba79d06
Alex <alexomaset711@g.mail.com>
2020-09-07 16:37:42 +0300

    initialized app icon

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.lightBlue,
         appBar: AppBar(
-          title: Text('I Am Rich'),
+          title: Center(child: Text('I Am Rich')),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
