6e3d749f3a52a8f8ac41449f883c37a2b57bc6dc
Alex <alexomaset711@g.mail.com>
2020-09-07 15:44:31 +0300

    add image to the application

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+          ),
         ),
       ),
     ),
