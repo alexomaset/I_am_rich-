47ecc0b5ff33524a78d830ba1705c8fcb0fac8d0
Alex <alexomaset711@g.mail.com>
2020-09-08 00:16:02 +0300

    add initialchanges

--- lib/main.dart
+++ lib/main.dart
@@ -15,34 +15,76 @@ class Myapp extends StatelessWidget {
           backgroundColor: Colors.blue[900],
         ),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/alex.jpg'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/alex.jpg'),
+                  ),
                 ),
-              ),
-              Text(
-                'Alex Omaset',
-                 style: TextStyle(
-                   fontSize: 40.0,
-                   color: Colors.white,
-                   fontWeight: FontWeight.bold,
+                Text(
+                  'Alex Omaset',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white, 
-                  fontWeight: FontWeight.bold,
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.amber,
+                      ),
+                      title: Text(
+                        '0727952163',
+                        style: TextStyle(
+                          color: Colors.teal.shade800,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.amber,
+                      ),
+                      title: Text(
+                        'alexomaset711@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade800,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
       ),
     );
   }
 }
+
